5d897668311821d97d3278ef945d1d09aee90f5d
AhmedElazony <ahmedazony2@gmail.com>
2026-04-29 00:40:25 +0300

    feat: add ui.

---
+++ frontend/src/components/AppButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+	text: { type: String, required: true },
+	icon: { type: String, default: null },
+	disabled: { type: Boolean, default: false }
+});
+defineEmits(['click']);
+</script>
+
+<template>
+	<button :disabled="disabled" @click="$emit('click')" class="btn-gradient disabled:opacity-50">
+		<span>{{ text }}</span>
+		<span v-if="icon">{{ icon }}</span>
+	</button>
+</template>
---
+++ frontend/src/components/AppCard.vue
@@ -0,0 +1,14 @@
+<template>
+	<div :class="[bordered ? 'card-bordered' : 'card']">
+		<slot></slot>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	bordered: {
+		type: Boolean,
+		default: false
+	}
+});
+</script>
