4e20016689fe6b36c4137fc7db67f5aa4d8a548d
tameesoul <tameeahmed77@gmail.com>
2026-04-30 02:33:36 +0300

    use Ar API messages

--- app/Domains/Transaction/Services/Database/TransactionService.php
+++ app/Domains/Transaction/Services/Database/TransactionService.php
@@ -46,7 +46,7 @@ class TransactionService implements TransactionServiceContract
                                   ->first();
 
             if (! $senderPivot) {
-                throw new RuntimeException('المستلم لا يملك هذا المحفظة مرتبطة بحسابه.');
+                throw new RuntimeException('الراسل لا يملك هذا المحفظة مرتبطة بحسابه.');
             }
 
             $fee = round($amount * self::FEE_PERCENTAGE, 2);
