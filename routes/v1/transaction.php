b0282a08ca305f8d11e45917e439a01c8c9a8386
AhmedElazony <ahmedazony2@gmail.com>
2026-04-30 02:45:03 +0300

    fix(api): route naming.

--- routes/v1/transaction.php
+++ routes/v1/transaction.php
@@ -3,10 +3,10 @@
 use App\Http\Api\V1\Controllers\Actions\Transaction\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1/transactions')->middleware('auth:sanctum')->group(function () {
+Route::prefix('transactions')->middleware('auth:sanctum')->group(function () {
     Route::post('/send', [TransactionController::class, 'send'])
-         ->name('transactions.send');
+        ->name('transactions.send');
 
     Route::get('/history', [TransactionController::class, 'history'])
-         ->name('transactions.history');
+        ->name('transactions.history');
 });
