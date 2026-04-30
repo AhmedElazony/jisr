5f6ca7aa926e7a09f6f270965161a9ba2c33d4fa
AhmedElazony <ahmedazony2@gmail.com>
2026-04-30 19:00:33 +0300

    fix(api): use channel, and auth for broadcasting.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+	App\Providers\BroadcastServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     App\Support\Providers\DomainServiceProvider::class,
 ];
--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Domains.User.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('transaction.{userId}', function ($user, $userId) {
+	return (int) $user->id === (int) $userId;
+});
